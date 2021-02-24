8760600f426c9e525e6bd3ddec7cbb12df95adb0
Wilson-Lo <lowilson180@gmail.com>
2021-02-24 11:45:48 +0800

    1. Add permission after FW update.

--- update.sh
+++ update.sh
@@ -7,3 +7,13 @@ wait
 echo "update end"
 sync
 echo "sync end"
+chmod 777 /home/linaro/Desktop/
+chmod 777 /home/linaro/Desktop/golang/
+chmod 777 /home/linaro/Desktop/golang/www
+chmod 777 /home/linaro/Desktop/golang/www/html/
+chmod 777 /home/linaro/Desktop/golang/www/js/
+chmod 777 /home/linaro/Desktop/golang/www/css/
+chmod 777 /home/linaro/Desktop/golang/www/img/
+chmod 777 /home/linaro/Desktop/golang/www/fonts/
+chmod 777 /home/linaro/Desktop/golang/www/webfonts/
+echo "set permission end"
